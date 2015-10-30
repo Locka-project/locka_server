a38c4d2b35121f3b92769c506f7563e79a2ce876
Alexis DUPUY <alexis.dupuy@y-nov.com>
2015-10-30 16:39:19 +0100

    add log monitoring in FrontUserControlller

--- api/controllers/FrontUserController.js
+++ api/controllers/FrontUserController.js
@@ -89,76 +89,70 @@ function FrontUserCtrl(){
         });
         res.redirect('/user');
       });
-		},
-		
-		delete: function (req, res) {
-			User.find({id: req.allParams().id}).exec(function foundCB(err, found) {
-				if (err) {
-						var log = "Error : " + err + " trying to find user with id " + req.allParams().id;
-						console.log(log);
-						return res;
-				}
-				if (found[0].id == null) {
-						var log = "No user with id " + req.allParams().id + ".";
-						console.log(log);
-						return res;
-				}
-				User.destroy({id: req.allParams().id}).exec(function deleteCB(err) {
-						if (err) {
-								var log = "Error : " + err + " trying to delete user " + found[0].name;
-								console.log(log);
-								return res;
-				}
-						var log = "User " + found[0].name + " correctly deleted.";
-						console.log(log);
-						return res;
-				});
-			});
-		},
+    },
 
-		forgetPassword: function (req, res) {
-				return res.view('user/forgetPassword');
-		},
+    delete: function (req, res) {
+      User.find({id: req.allParams().id}).exec(function foundCB(err, found) {
+        if (err) {
+          LogService.create({type: "Error", description: "Error : " + err + " trying to find user with id " + req.allParams().id});
+          console.log(log);
+          return res;
+        }
+        if (found[0].id == null) {
+          LogService.create({type: "Error", description: "No user with id " + req.allParams().id + "."});
+          console.log(log);
+          return res;
+        }
+        User.destroy({id: req.allParams().id}).exec(function deleteCB(err) {
+          if (err) {
+            LogService.create({type: "Error", description: "Error : " + err + " trying to delete user " + found[0].name});
+            console.log(log);
+            return res;
+          }
+          LogService.create({type: "Delete", description: "User " + found[0].name + " correctly deleted."});
+          console.log(log);
+          return res;
+        });
+      });
+    },
 
-		sendNewPassword: function(req, res){
-			User.find({email:req.allParams().email}).exec(function findCB(err, found){
-				if(err){
-					res = "Error : " + err + " trying to find user.";
-					console.log(res);
-					return res;
-				}
-	
-				var newPassword = generatePassword();
-	      findPassport(found[0], res, function(err, passport){
-	        if(err){
-	          res = "Error : " + err + " trying to find user.";
-	          console.log(res);
-	          return res;
-	        }
-	        passport.password = newPassword;
-	        passport.save();
-	      });
-	
-	      var response = EmailService.forgetPassword(found[0], newPassword);
-	      if(response != true){
-	        res = "Error : " + err + " trying to send mail.";
-	        console.log(res);
-	        return res;
-	      }
-	
-				return res.redirect('/login');
-			});
-		},
+    forgetPassword: function (req, res) {
+      return res.view('user/forgetPassword');
+    },
+
+    sendNewPassword: function(req, res){
+      User.find({email:req.allParams().email}).exec(function findCB(err, found){
+        if(err){
+          LogService.create({type: "Error", description: "Error : " + err + " trying to find user."});
+          return res;
+        }
+
+        var newPassword = generatePassword();
+        findPassport(found[0], res, function(err, passport){
+          if(err){
+            LogService.create({type: "Error", description: "Error : " + err + " trying to find user."});
+            return res;
+          }
+          passport.password = newPassword;
+          passport.save();
+        });
+
+        var response = EmailService.forgetPassword(found[0], newPassword);
+        if(response != true){
+          LogService.create({type: "Error", description: "Error : " + err + " trying to send mail."});
+          return res;
+        }
+
+        return res.redirect('/login');
+      });
+    },
 
     getAllUsers: function (req, res) {
       User.find({}).exec(function findCB(err, found) {
         if (err) {
-          var log = "Error : " + err + " trying to list all users.";
-          console.log(log);
+          LogService.create({type: "Error", description: "Error : " + err + " trying to list all users."});
           return res;
         }
-        var log = "Users correctly listed.";
-        console.log(log);
         return res.json(found);
       });
     },
@@ -166,23 +160,20 @@ function FrontUserCtrl(){
     getDevicesByUser: function (req, res) {
       User.find({id: req.allParams().id}).populate('deviceList').exec(function (err, devices) {
         if (err) {
-          var log = "Error : " + err + " trying to list all users.";
-          console.log(log);
+          LogService.create({type: "Error", description: "Error : " + err + " trying to list all users."});
           return res;
         }
-        var log = "Users correctly deleted.";
-        console.log(log);
         return res.json(devices);
       })
     },
-    
+
     subscribe: function(req,res){
-			if(req.req.isSocket){
-				var deviceList = getDevicesByUser(req.user.id);
-				
-				sails.log(deviceList);
-			}
-		},
+      if(req.req.isSocket){
+        var deviceList = getDevicesByUser(req.user.id);
+
+        sails.log(deviceList);
+      }
+    },
   }
 }
 module.exports = FrontUserCtrl();
