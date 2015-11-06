abb4df3a1b0b860c603f5606a7b757d0f1a2284b
Alexis DUPUY <alexis.dupuy@y-nov.com>
2015-11-06 12:26:24 +0100

    init fronLogController getLogByUser

--- api/controllers/FrontLogController.js
+++ api/controllers/FrontLogController.js
@@ -7,19 +7,33 @@
 
 function LogCtrl(){
 
-    return{
-      getLogByDevice: function(req, res){
-        Log.find({id : req.allParams().id}).exec(function findLog(err, logList){
-          if(err) {
-            var log = "Error : " + err + " trying to list log by device.";
-            console.log(log);
-            return res;
-          }
-          var log = "Log correctly listed."
+  return{
+    getLogByDevice: function(req, res){
+      Log.find({id : req.allParams().id}).exec(function findLog(err, logList){
+        if(err) {
+          var log = "Error : " + err + " trying to list log by device.";
           console.log(log);
+          return res;
+        }
+        var log = "Log correctly listed."
+        console.log(log);
+        return res.json(logList);
+      });
+    },
+    getLogsByUser: function(req,res) {
+      Log.find({user_id: req.user.id}).exec(function finLogCB(err, logList) {
+        if(err) {
+          var log = "Error : " + err + " trying to list log by user.";
+          console.log(log);
+          return err;
+        }
+        if(!req.isSocket) {
           return res.json(logList);
-        });
-      }
+        } else {
+          
+        }
+      });
     }
+  }
 }
 module.exports = LogCtrl();
